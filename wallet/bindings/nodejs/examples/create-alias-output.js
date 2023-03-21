dc86bae83a5e36ca3e408a42a56a2f074915051a
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-03-21 14:34:05 +0100

    Add NodeJS example

---
+++ wallet/bindings/nodejs/examples/create-alias-output.js
@@ -0,0 +1,23 @@
+/**
+ * This example mints native tokens
+ */
+const getUnlockedManager = require('./account-manager');
+
+async function run() {
+    try {
+        const manager = await getUnlockedManager();
+
+        const account = await manager.getAccount('0');
+
+        await account.sync();
+
+        // First create an alias output, this needs to be done only once, because an alias can have many foundry outputs.
+        let tx = await account.createAliasOutput()
+        console.log('Transaction ID: ', tx.transactionId);
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+    process.exit(0);
+}
+
+run();
