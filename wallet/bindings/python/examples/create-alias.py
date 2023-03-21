c97a64d35c4a6645169901e7564017c017c301d3
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-03-21 23:40:38 +0100

    Refactor user provided values and output

--- wallet/bindings/python/examples/create-alias.py
+++ wallet/bindings/python/examples/create-alias.py
@@ -1,17 +1,19 @@
 from iota_wallet import IotaWallet
 
-# In this example we will mint native tokens
+# In this example we will create an alias ouput
+
+# TODO: replace with your own values.
+WALLET_PASSWORD = "some_hopefully_secure_password"
 
 wallet = IotaWallet('./alice-database')
 
 account = wallet.get_account('Alice')
 
 # Sync account with the node
-response = account.sync()
-print(f'Synced: {response}')
+account.sync()
 
-wallet.set_stronghold_password("some_hopefully_secure_password")
+wallet.set_stronghold_password(WALLET_PASSWORD)
 
+# Send transaction.
 transaction = account.create_alias_output(None, None)
-
-print(f'Sent transaction: {transaction}')
+print(f'Sent transaction: {transaction["transactionId"]}')
--- wallet/bindings/python/examples/destroy_alias.py
+++ wallet/bindings/python/examples/destroy_alias.py
@@ -1,7 +1,10 @@
 from iota_wallet import IotaWallet
-import time
 
-# In this example we will destroy an alias
+# In this example we will destroy an alias output
+
+# TODO: replace with your own values.
+ALIAS_ID = "0x08429fe5864378ce70699fc2d22bb144cb86a3c4833d136e3b95c5dadfd6ba0cef0500000000"
+WALLET_PASSWORD = "some_hopefully_secure_password"
 
 wallet = IotaWallet('./alice-database')
 
@@ -9,19 +12,9 @@ account = wallet.get_account('Alice')
 
 # Sync account with the node
 response = account.sync()
-print(f'Balance before destroying:\n{response}')
 
-wallet.set_stronghold_password("some_hopefully_secure_password")
-
-# TODO: replace with your own values.
-alias_id = "0x08429fe5864378ce70699fc2d22bb144cb86a3c4833d136e3b95c5dadfd6ba0cef0500000000"
+wallet.set_stronghold_password(WALLET_PASSWORD)
 
 # Send transaction.
-account.destroy_alias(alias_id)
-
-# Wait a few seconds for the transaction to get confirmed
-time.sleep(7)
-
-# Sync account with the node
-response = account.sync()
-print(f'Balance after destroying:\n{response}')
+transaction = account.destroy_alias(alias_id)
+print(f'Sent transaction: {transaction["transactionId"]}')
