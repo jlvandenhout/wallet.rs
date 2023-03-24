06a82335c1be567cdfc56aa77109c94b2ec7dd27
Jeroen van den Hout <jlvandenhout@gmail.com>
2023-03-24 22:11:13 +0100

    Update destroy example and add to docs

--- wallet/bindings/nodejs/examples/destroy-alias.js
+++ wallet/bindings/nodejs/examples/destroy-alias.js
@@ -4,25 +4,18 @@
 const getUnlockedManager = require('./account-manager');
 
 async function run() {
+    // Replace with an existing alias ID in your account
+    const ALIAS_ID = '0x08e6210d29881310db2afde095e594f6f006fcdbd06e7a83b74bd2bdf3b5190d0e0200000000'
+    
     try {
         const manager = await getUnlockedManager();
 
         const account = await manager.getAccount('0');
 
         await account.sync();
-        console.log('Balance before destroying:\n', await account.getBalance());
 
-        // Replace with an existing alias ID in your account
-        let aliasId =
-            '0x08e6210d29881310db2afde095e594f6f006fcdbd06e7a83b74bd2bdf3b5190d0e0200000000';
-
-        await account.destroyAlias(aliasId);
-
-        // Wait for transaction inclusion
-        await new Promise(resolve => setTimeout(resolve, 5000));
-
-        await account.sync();
-        console.log('Balance after destroying:\n', await account.getBalance());
+        let tx = await account.destroyAlias(ALIAS_ID);
+        console.log(tx);
     } catch (error) {
         console.log('Error: ', error);
     }
